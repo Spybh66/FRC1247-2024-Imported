ee5da0c00ebc65010a6b1022e66055fd1b175e01
spybh66 <brysonh99@gmail.com>
2024-02-28 16:47:45 -0500

    Gyro work, not finished

---
+++ src/main/java/frc/robot/GyroSensor.java
@@ -0,0 +1,26 @@
+package frc.robot;
+import edu.wpi.first.wpilibj.I2C;
+
+
+public class GyroSensor {
+    private static double degrees = 0;
+	
+	private I2C gyro = new I2C(I2C.Port.kOnboard, Constants.IMU.GYRO_ADDRESS);
+	
+	public GyroSensor() {
+		gyro.write(0x6B, 0x03); // Power
+		gyro.write(0x1A, 0x18); // Basic Config
+		gyro.write(0x1B, 0x00); // Gyro Config
+	}
+	
+	public double getDegrees (double deltaTime) {
+		byte[] angle = new byte[1];
+		gyro.read(0x47, 1, angle);
+		
+		double rotation = (angle[0] * deltaTime) * 2;
+		
+		degrees += rotation;
+		
+		return -degrees;
+	}
+}
